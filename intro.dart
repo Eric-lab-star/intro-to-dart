5393a38a68136118f7b4f560fbbd8aa088350269
eric-lab-star <cyon256@icloud.com>
2023-03-23 16:17:01 +0900

    comments

--- intro.dart
+++ intro.dart
@@ -1,9 +1,17 @@
 void main() {
-  late int number = shoutNumber();
-
-  print(number);
+  Television.activate();
 }
 
-int shoutNumber() {
-  return 10;
+class Television {
+  /// Use [turnOn] to turn the power on instead.
+  @Deprecated('Use turnOn instead')
+  static void activate() {
+    turnOn();
+  }
+
+  /// Turns the TV's power on.
+  static void turnOn() {
+    print('Turn on TV');
+  }
+  // ···
 }
