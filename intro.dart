a08c4f3d5b2bfe6bf63b805b69d3fd3524dc84fe
eric-lab-star <cyon256@icloud.com>
2023-03-29 19:43:57 +0900

    quickstart

--- intro.dart
+++ intro.dart
@@ -1,24 +1,5 @@
 void main() {
-  Television.turnOn();
-}
-
-class Television {
-  /// Use [turnOn] to turn the power on instead.
-  @Deprecated('Use turnOn instead')
-  static void activate() {
-    turnOn();
-  }
-
-  @Todo('me', "code")
-  static void turnOn() {
-    print('Turn on TV');
-  }
-  // ···
-}
-
-class Todo {
-  final String who;
-  final String what;
-
-  const Todo(this.who, this.what);
+  var number = 1;
+  number = 2;
+  print(number);
 }
---
+++ quickstart/web/main.dart
@@ -0,0 +1,20 @@
+import 'dart:html';
+
+void main() {
+  querySelector('#output')?.children.addAll(thingsTodo().map(newLI));
+}
+
+Iterable<String> thingsTodo() sync* {
+  const actions = ['Walk', 'Wash', 'Feed'];
+  const pets = ['cats', 'dogs'];
+
+  for (final action in actions) {
+    for (final pet in pets) {
+      if (pet != 'cats' || action == 'Feed') {
+        yield '$action the $pet';
+      }
+    }
+  }
+}
+
+LIElement newLI(String itemText) => LIElement()..text = itemText;
